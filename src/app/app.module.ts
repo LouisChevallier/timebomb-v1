568efd91e25d630f6a01f2da118c6a46a3d2e77a
Louis Chevallier <louischevallier@MacBook-Pro-de-Louis.local>
2020-06-25 15:27:37 +0200

    Ajout navbar & SPA works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PlateauComponent } from './plateau/plateau.component';
+
+
+// Configuration des routes
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'plateau', component: PlateauComponent },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    PlateauComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
